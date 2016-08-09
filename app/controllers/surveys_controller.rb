04839d0307e53ee404060697f491cd969eb0b7c2
“Matthew <matthew.hinea@gmail.com>
2016-08-08 21:19:18 -0700

    nested question fields now submitting, saving, and rendering correctly

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -7,13 +7,41 @@ class SurveysController < ApplicationController
   end
 
   def create
-    @survey = Survey.new(p)
+    @survey = Survey.new(whitelist)
+    if @survey.save
+      flash[:success] = "Your survey was created."
+      redirect_to @survey
+    else
+      flash.now[:errors] = "Your survey didn't create."
+      render :new
+    end
+  end
+
+  def edit
+    @survey = Survey.find(params[:id])
+    @questions = @survey.questions
+    @survey.questions.build
+  end
+
+  def show
+    @survey = Survey.find(params[:id])
+  end
+
+  def update
+    @survey = Survey.find(params[:id])
+    if @survey.update(whitelist)
+      flash[:success] = "Your survey and questions have been updated."
+      redirect_to @survey
+    else
+      flash.now[:errors] = "Your changes were not saved for some reason."
+      render :edit
+    end    
   end
 
 
   private 
 
-  def white_list
-    params.require(:survey).permit(:name, :description)
+  def whitelist
+    params.require(:survey).permit(:name, :description, :questions_attributes => [:id, :text, :_destroy])
   end
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,5 @@
 class Survey < ActiveRecord::Base
   has_many :questions
+  accepts_nested_attributes_for :questions, :reject_if => :all_blank
   has_many :answers, through: :questions
 end
